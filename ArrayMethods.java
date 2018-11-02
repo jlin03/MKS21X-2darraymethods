99b3aeaf897114c44598abcfe746b9a7f05b1db2
jlin03 <jlin03@stuy.edu>
2018-11-02 10:00:39 -0400

    rowSum method coded

---
+++ ArrayMethods.java
@@ -0,0 +1,18 @@
+public class ArrayMethods {
+
+  public static int rowSum(int[][] ary, int x) {
+    int count = 0;
+    for(int i = 0; i < ary[x].length; i++) {
+      count += ary[x][i];
+    }
+  }
+
+
+
+
+
+
+
+
+
+}
