44c09a9248a5d7f03357902c59fd728ec0672978
jlin03 <jlin03@stuy.edu>
2018-11-02 10:08:53 -0400

    test cases added for rowSUm and columnSUm

---
+++ Driver.java
@@ -0,0 +1,16 @@
+public class Driver {
+  public static void main(String[] args) {
+    int[][] a = new int[4][4];
+    for(int i = 0; i < 4;i++) {
+      for(int x = 0; x < 4;x++) {
+        a[i][x] = i + 1;
+      }
+    }
+    System.out.println(ArrayMethods.rowSum(a,2));
+    //12
+    System.out.println(ArrayMethods.columnSum(a,0));
+    //10
+
+  }
+
+}
