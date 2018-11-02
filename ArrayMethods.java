01711398cf1119b310404125b80e9dd7eb6d2717
jlin03 <jlin03@stuy.edu>
2018-11-02 10:15:38 -0400

    allrow and allcol sum methods added, test cases to come

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -18,7 +18,27 @@ public class ArrayMethods {
     return count;
   }
 
+  public static int[] allRowSums(int[][] ary) {
+    int[] out = new int[ary.length];
+    for(int i = 0; i < ary.length;i++) {
+      out[i] = rowSum(ary, i);
+    }
+    return out;
+  }
 
+  public static int[] allColSums(int[][] ary) {
+    int[] out = new int[ary.length];
+    int maxRowSize = 0;
+    for(int i = 0; i < ary.length;i++) {
+      if(ary[i].length > maxRowSize) {
+        maxRowSize = ary[i].length;
+      }
+    }
+    for(int i = 0; i < maxRowSize;i++) {
+      out[i] = columnSum(ary, i);
+    }
+    return out;
+  }
 
 
 
